2df86d5fc1be3bad034b8d93f5484d2d7bebf0f8
David <davidpal3c@gmail.com>
2025-06-18 07:42:15 -0600

    backend cors middleware update

--- nzeroesg-api/main.py
+++ nzeroesg-api/main.py
@@ -4,18 +4,18 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# origins = [
-#     "http://localhost:5173",    
-#     "http://127.0.0.1:5173",
-# ]
+origins = [
+    "https://nzeroesg-client.onrender.com",
+    "http://localhost:5173",    
+    "http://127.0.0.1:5173",
+]
 
 app.add_middleware(
-    CORSMiddleware,
-    # allow_origins=["*"],                                # Changing to specific origins in production
-    allow_origins=["https://nzeroesg-api.onrender.com", "http://localhost:8000"],                                # Changing to specific origins in production
-    allow_credentials=True,                             # Allow credentials (cookies, authorization headers, etc.)   
-    allow_methods=["*"],                                # Allow all HTTP methods
-    allow_headers=["*"],                                # Allow all headers
+    CORSMiddleware,                              
+    allow_origins=origins,                                
+    allow_credentials=True,                             
+    allow_methods=["*"],                                
+    allow_headers=["*"],                                
 )
 
 app.include_router(chat_router, prefix="/chat", tags=["chat"])
